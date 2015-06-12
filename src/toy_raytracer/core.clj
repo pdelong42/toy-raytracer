72d52b398a67bce9985dff3f1aedbb4cedb24bab
Paul DeLong <Paul.DeLong@gmail.com>
2015-06-12 00:19:07 -0400

    minor oversight;

--- src/toy_raytracer/core.clj
+++ src/toy_raytracer/core.clj
@@ -13,9 +13,9 @@
 (defn distance
    [p1 p2]
    (mag
-      (- (:x p1) (:x p2))
-      (- (:y p1) (:y p2))
-      (- (:z p1) (:z p2))  )  )
+      [  (- (:x p1) (:x p2))
+         (- (:y p1) (:y p2))
+         (- (:z p1) (:z p2))  ]  )  )
 
 (defn minroot
    [a b c]
