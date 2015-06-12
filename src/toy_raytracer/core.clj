3d6a411a8b3a3d82996de09c68ec99ade4838de8
Paul DeLong <Paul.DeLong@gmail.com>
2015-06-12 00:15:33 -0400

    math utilities, adapted from fig 9.2 in ACL;

--- src/toy_raytracer/core.clj
+++ src/toy_raytracer/core.clj
@@ -1,7 +1,39 @@
-(ns toy-raytracer.core (:gen-class))
+(ns toy-raytracer.core
+   (:import java.lang.Math)
+   (:gen-class)  )
+
+(defn square [x] (* x x))
+
+(defn mag [v] (Math/sqrt (reduce + (map square v))))
+
+(defn unit-vector [v] (map #(/ % (mag v)) v))
+
+(defrecord Point [x y z])
+
+(defn distance
+   [p1 p2]
+   (mag
+      (- (:x p1) (:x p2))
+      (- (:y p1) (:y p2))
+      (- (:z p1) (:z p2))  )  )
+
+(defn minroot
+   [a b c]
+   (if
+      (zero? a)
+      (/ (- c) b)
+      (let
+         [  disc (- (square b) (* 4 a c))  ]
+         (if-not
+            (> disc 0)
+            (let
+               [  discrt (Math/sqrt disc)  ]
+               (min
+                  (/ (+ (- b) discrt) (* 2 a))
+                  (/ (- (- b) discrt) (* 2 a))  )  )  )  )  )  )
 
 (defn -main
-  [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+   [& args]
+   ;; work around dangerous default behaviour in Clojure
+   (alter-var-root #'*read-eval* (constantly false))
+   (println "Hello, World!"))
