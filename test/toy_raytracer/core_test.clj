f99b90271d979b951aab56672465a59add89d02e
Paul DeLong <Paul.DeLong@gmail.com>
2015-07-02 17:45:58 -0400

    comment to remind myself to implement a test later;

--- test/toy_raytracer/core_test.clj
+++ test/toy_raytracer/core_test.clj
@@ -11,3 +11,6 @@
 
 (deftest type-of-sphere
    (is (= toy_raytracer.core.Sphere (type (toy_raytracer.core.Sphere. (toy_raytracer.core.Surface. :red) 5 (toy_raytracer.core.Point. 1 2 3)))))  )
+
+;toy-raytracer.core=> (normal (Sphere. (Surface. :red) 5 (Point. 1 2 3)) (Point. 4 5 6))
+;(-0.5773502691896257 -0.5773502691896257 -0.5773502691896257)
