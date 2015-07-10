ce0b11b2a870c467f11b26f55a37083729399485
Paul DeLong <Paul.DeLong@gmail.com>
2015-07-10 10:12:28 -0400

    fixed the rest of the fully-qualified symbols to be relative;

--- test/toy_raytracer/core_test.clj
+++ test/toy_raytracer/core_test.clj
@@ -5,20 +5,16 @@
    (  :import
       [toy_raytracer.core Surface]  )  )
 
-(deftest type-of-surface
-   (is
-      (= Surface
-         (type
-            (toy_raytracer.core.Surface. :red))))  )
+(deftest type-of-surface (is (= Surface (type (->Surface :red)))))
 
 (deftest type-of-sphere
    (is
-      (= toy_raytracer.core.Sphere
+      (= Sphere
          (type
-            (toy_raytracer.core.Sphere.
-               (toy_raytracer.core.Surface. :red)
+            (->Sphere
+               (->Surface :red)
                5
-               (toy_raytracer.core.Point. 1 2 3)  )  )  )  )  )
+               (->Point 1 2 3)  )  )  )  )  )
 
 ;toy-raytracer.core=> (normal (Sphere. (Surface. :red) 5 (Point. 1 2 3)) (Point. 4 5 6))
 ;(-0.5773502691896257 -0.5773502691896257 -0.5773502691896257)
