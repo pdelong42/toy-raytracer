f4a8c6ec58bbb76bff9636c632f6bd625ee6ca30
Paul DeLong <Paul.DeLong@gmail.com>
2015-07-10 08:50:19 -0400

    finally figured-out what I was doing wrong that necessitated me fully qualifying symbols - fixed by using import; expanded my 2nd test with some whitespace;

--- test/toy_raytracer/core_test.clj
+++ test/toy_raytracer/core_test.clj
@@ -1,16 +1,24 @@
 (ns toy_raytracer.core-test
    (  :require
       [clojure.test       :refer :all]
-      [toy-raytracer.core :refer :all]  )  )
+      [toy-raytracer.core :refer :all]  )
+   (  :import
+      [toy_raytracer.core Surface]  )  )
 
 (deftest type-of-surface
    (is
-      (= toy_raytracer.core.Surface
+      (= Surface
          (type
             (toy_raytracer.core.Surface. :red))))  )
 
 (deftest type-of-sphere
-   (is (= toy_raytracer.core.Sphere (type (toy_raytracer.core.Sphere. (toy_raytracer.core.Surface. :red) 5 (toy_raytracer.core.Point. 1 2 3)))))  )
+   (is
+      (= toy_raytracer.core.Sphere
+         (type
+            (toy_raytracer.core.Sphere.
+               (toy_raytracer.core.Surface. :red)
+               5
+               (toy_raytracer.core.Point. 1 2 3)  )  )  )  )  )
 
 ;toy-raytracer.core=> (normal (Sphere. (Surface. :red) 5 (Point. 1 2 3)) (Point. 4 5 6))
 ;(-0.5773502691896257 -0.5773502691896257 -0.5773502691896257)
