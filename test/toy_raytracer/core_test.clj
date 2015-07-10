ec5886f848164ef608f5b75b0cb2c60923699d19
Paul DeLong <Paul.DeLong@gmail.com>
2015-07-10 10:37:25 -0400

    new test, doesn't pass yet;

--- test/toy_raytracer/core_test.clj
+++ test/toy_raytracer/core_test.clj
@@ -7,6 +7,8 @@
 
 (def dummy-point (->Point 1 2 3))
 
+(def dummy-ray (->Point 4 5 6))
+
 (def red-surface (->Surface :red))
 
 (def red-sphere (->Sphere red-surface 5 dummy-point))
@@ -15,5 +17,4 @@
 
 (deftest type-of-sphere (is (= Sphere (type red-sphere))))
 
-;toy-raytracer.core=> (normal (Sphere. (Surface. :red) 5 (Point. 1 2 3)) (Point. 4 5 6))
-;(-0.5773502691896257 -0.5773502691896257 -0.5773502691896257)
+(deftest normal-test (is (= 0.0 (- 1 (square (normal red-sphere dummy-ray))))))
