cdbb6800638291cbf79ba9f57b58559f4d681171
Paul DeLong <Paul.DeLong@gmail.com>
2015-07-10 10:24:47 -0400

    one more convenience item;

--- test/toy_raytracer/core_test.clj
+++ test/toy_raytracer/core_test.clj
@@ -5,14 +5,15 @@
    (  :import
       [toy_raytracer.core Sphere Surface]  )  )
 
+(def dummy-point (->Point 1 2 3))
+
 (def red-surface (->Surface :red))
 
-(def dummy-point (->Point 1 2 3))
+(def red-sphere (->Sphere red-surface 5 dummy-point))
 
 (deftest type-of-surface (is (= Surface (type red-surface))))
 
-(deftest type-of-sphere
-   (is (= Sphere (type (->Sphere red-surface 5 dummy-point))))  )
+(deftest type-of-sphere (is (= Sphere (type red-sphere))))
 
 ;toy-raytracer.core=> (normal (Sphere. (Surface. :red) 5 (Point. 1 2 3)) (Point. 4 5 6))
 ;(-0.5773502691896257 -0.5773502691896257 -0.5773502691896257)
