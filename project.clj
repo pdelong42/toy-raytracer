b9b2f0e714ceff599b6095fce4c45d35c7d0b27e
Paul DeLong <Paul.DeLong@gmail.com>
2015-07-29 18:29:09 -0400

    [bugfix/2.0.1] version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject toy-raytracer "2.0.0"
+(defproject toy-raytracer "2.0.1"
    :license {:name "MIT License"}
    :dependencies [[org.clojure/clojure "1.5.1"]]
    :main toy-raytracer.core  )
