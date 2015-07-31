dc582b72b858947562f0914fb68d9b5d63f70187
Paul DeLong <Paul.DeLong@gmail.com>
2015-07-31 18:12:58 -0400

    [feature/command-line-args] first stab
    I just air-lifted my command-line code from url-looper, ripped-out a
    bunch of options, and added the sole option I want to support for
    starters.  Easier than I thought it would be.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject toy-raytracer "2.0.1"
    :license {:name "MIT License"}
-   :dependencies [[org.clojure/clojure "1.5.1"]]
+   :dependencies
+   [  [org.clojure/clojure   "1.5.1"]
+      [org.clojure/tools.cli "0.3.2"]  ]
    :main toy-raytracer.core  )
